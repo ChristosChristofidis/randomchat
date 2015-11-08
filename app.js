9dd5503aaeafe509e4c7b468b95c232ac17e0422
Christos <christofidis.c@student.unic.ac.cy>
2015-11-08 22:21:17 +0200

    functions for randomchat

--- app.js
+++ app.js
@@ -22,12 +22,45 @@ app.get('/',function(req,res){
 });
 
 
-var arrayofpeople = []
+var arrayofpeople = [];
+var sessionarray = [];
 io.on('connection',function(client){
-    client.emit('notconnected',{hello:'s',array:arrayofpeople});
-    arrayofpeople.push(client.request.headers.cookie.substring(67, 120)); //hotfix 120
-    console.log(arrayofpeople);
-    console.log(arrayofpeople.length);
+    client.on('lookingforstranger',function(data){
+        var mycookie = client.request.headers.cookie.substring(67, 100);
+        arrayofpeople.push(mycookie); //hotfix 120
+
+        var setofpeople = new Set(arrayofpeople);
+        setofpeople.forEach(function(a,person){
+            console.log('person : ' + typeof(person) + ' ' + person);
+            console.log('person 2nd : ' + a);
+            console.log('mycookie : ' + typeof(mycookie) + ' ' + mycookie);
+            if (person != mycookie){
+                var sessionID = Math.random();
+                sessionarray.push([mycookie,person,sessionID]);
+                console.log('I was there and also : ' + typeof(sessionID) + ' ' + sessionID );
+                client.emit('sessionID', {sessionID: sessionID});
+                return false;
+
+            }
+        });
+        try {
+            client.emit('sessionID', {sessionID: sessionID});
+            console.log('i tried and succeeded');
+        }
+        catch(e){console.log('i catched it');}
+        console.log(setofpeople);
+        console.log(arrayofpeople.length);
+
+
+
+
+    });
+
+    client.on('messages',function(data){
+
+
+        client.broadcast.emit('message',{message:data.message,sessionID:data.sessionID});
+    });
 
 });
 
